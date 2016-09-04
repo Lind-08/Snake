b149fd587637cea184e498632686a9711c08dc5a
lind-08 <lind8584@gmail.com>
2016-09-04 15:18:53 +0300

    Продолжение урока

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -15,6 +15,12 @@ namespace Snake
             char sym = '*';
             Console.SetCursorPosition(x1, y1);
             Console.Write(sym);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
             Console.ReadKey();
         }
     }
