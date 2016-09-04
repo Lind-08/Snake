d2e572987825324413b26be3680808efbec7b23e
lind-08 <lind8584@gmail.com>
2016-09-04 15:21:46 +0300

    Код рисования символа вынесен в отдельную функцию

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,16 +12,21 @@ namespace Snake
         {
             int x1 = 1;
             int y1 = 3;
-            char sym = '*';
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym);
+            char sym1 = '*';
+            WriteSymbol(x1, y1, sym1);
 
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
+            WriteSymbol(x2, y2, sym2);
+
             Console.ReadKey();
+        }
+
+        private static void WriteSymbol(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
