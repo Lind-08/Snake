48992e6ea7b4ff497115d2c974e3689525fa6f47
lind-08 <lind8584@gmail.com>
2016-09-11 15:04:58 +0300

    Добавлен класс фигуры, внесено наследование.

--- Snake/HorizontalLine.cs
+++ Snake/HorizontalLine.cs
@@ -6,24 +6,14 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    internal class HorizontalLine
+    internal class HorizontalLine : Figure
     {
-        private List<Point> pList;
-
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
             for (int x = xLeft; x <= xRight; x++)
             {
                 pList.Add(new Point(x, y, sym));
-            }
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
             }
         }
     }
--- Snake/VerticalLine.cs
+++ Snake/VerticalLine.cs
@@ -6,24 +6,14 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    internal class VerticalLine
+    internal class VerticalLine : Figure
     {
-        private List<Point> pList;
-
         public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
             for (int y = yUp; y <= yDown; y++)
             {
                 pList.Add(new Point(x, y, sym));
-            }
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
             }
         }
     }
