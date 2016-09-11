a9a7aa6f712c80a0213308faedd61e46e6ae4aec
lind-08 <lind8584@gmail.com>
2016-09-11 14:57:58 +0300

    Добавлено рисование рамки

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,16 +6,20 @@ namespace Snake
     {
         private static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Console.SetWindowPosition(0, 0);
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            HorizontalLine line = new HorizontalLine(4, 6, 4, '*');
-            line.Draw();
-
-            VerticalLine line1 = new VerticalLine(4, 6, 3, '+');
-            line1.Draw();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
             Console.ReadKey();
         }
     }
